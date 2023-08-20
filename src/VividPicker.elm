a6d1d508b926cada2d337d79e30313ed8f0dec97
Yoshitaka Totsuka <y047aka@gmail.com>
2023-08-20 12:22:31 +0900

    refactor

--- src/VividPicker.elm
+++ src/VividPicker.elm
@@ -1,48 +1,45 @@
 module VividPicker exposing (hsl, oklch)
 
 import Css exposing (..)
-import HSL exposing (hslSteps)
 import Html.Styled exposing (Html, div)
 import Html.Styled.Attributes exposing (css)
-import Oklch exposing (Oklch)
-
-
-
--- HSL
+import Oklch
 
 
 hsl : Html msg
 hsl =
-    div
-        [ css
-            [ property "display" "grid"
-            , property "grid-template-columns" "repeat(361, 1fr)"
-            ]
-        ]
-        (List.map (div []) <|
-            List.map cell_hsl HSL.monoSteps
-                :: List.map (\hue -> List.map cell_hsl (hslSteps { hue = toFloat hue * 1 })) (List.range 0 359)
-        )
+    let
+        monoSteps =
+            List.map (\l -> Css.hsl 0 0 l) (range 0 1 0.005)
+                |> List.reverse
 
-
-cell_hsl : Color -> Html msg
-cell_hsl hslColor =
-    div
-        [ css
-            [ padding2 (px 1) zero
-            , backgroundColor hslColor
-            , fontFamily sansSerif
-            ]
-        ]
-        []
-
-
-
--- OKLCH
+        hslSteps { hue } =
+            List.map (\l -> Css.hsl hue 1 l) (List.reverse <| range 0 1 0.005)
+    in
+    vividPicker
+        { monoSteps = monoSteps
+        , toColorSteps = hslSteps
+        }
 
 
 oklch : Html msg
 oklch =
+    let
+        monoSteps =
+            List.map (\l -> Oklch.oklch l 0 0) (range 0 1 0.005)
+                |> List.reverse
+
+        toColorSteps { hue } =
+            List.map (\l -> Oklch.oklch l 0.2 hue) (List.reverse <| range 0 1 0.005)
+    in
+    vividPicker
+        { monoSteps = monoSteps
+        , toColorSteps = toColorSteps
+        }
+
+
+vividPicker : { monoSteps : List Color, toColorSteps : { hue : Float } -> List Color } -> Html msg
+vividPicker { monoSteps, toColorSteps } =
     div
         [ css
             [ property "display" "grid"
@@ -50,32 +47,21 @@ oklch =
             ]
         ]
         (List.map (div []) <|
-            List.map cell_oklch monoSteps_oklch
-                :: List.map (\hue -> List.map cell_oklch (oklchSteps { hue = toFloat hue * 1 })) (List.range 0 359)
+            List.map cell monoSteps
+                :: List.map (\hue -> List.map cell (toColorSteps { hue = toFloat hue * 1 })) (List.range 0 359)
         )
 
 
-cell_oklch : Oklch -> Html msg
-cell_oklch { luminance, chroma, hue } =
+cell : ColorValue compatible -> Html msg
+cell color =
     div
         [ css
             [ padding2 (px 1) zero
-            , backgroundColor (Oklch.oklch luminance chroma hue)
+            , backgroundColor color
             , fontFamily sansSerif
             ]
         ]
         []
-
-
-oklchSteps : { hue : Float } -> List Oklch
-oklchSteps { hue } =
-    List.map (\l -> Oklch l 0.2 hue) (List.reverse <| range 0 1 0.005)
-
-
-monoSteps_oklch : List Oklch
-monoSteps_oklch =
-    List.map (\l -> Oklch l 0 0) (range 0 1 0.005)
-        |> List.reverse
 
 
 
