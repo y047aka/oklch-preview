3df74c2d92e62eb8de4467005d38e77907c6f611
Yoshitaka Totsuka <y047aka@gmail.com>
2023-09-02 11:04:37 +0900

    refactor: remove unused ports

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-port module Main exposing (main)
+module Main exposing (main)
 
 import Browser exposing (Document)
 import Css exposing (..)
@@ -13,18 +13,8 @@ main =
         { init = init
         , update = update
         , view = view
-        , subscriptions = subscriptions
+        , subscriptions = \_ -> Sub.none
         }
-
-
-
--- PORTS
-
-
-port sendMessage : { monoSteps : List Okhsl, colorSteps : List (List Okhsl) } -> Cmd msg
-
-
-port messageReceiver : ({ monoSteps : List (List Float), colorSteps : List (List (List Float)) } -> msg) -> Sub msg
 
 
 
@@ -32,65 +22,27 @@ port messageReceiver : ({ monoSteps : List (List Float), colorSteps : List (List
 
 
 type alias Model =
-    { monoSteps : List Color
-    , colorSteps : List (List Color)
-    }
-
-
-type alias Okhsl =
-    { h : Float, s : Float, l : Float }
+    {}
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { monoSteps = [], colorSteps = [] }
-    , sendMessage
-        { monoSteps =
-            reverseRange 0 1 0.005
-                |> List.map (\l -> Okhsl 0 0 l)
-        , colorSteps =
-            List.range 0 359
-                |> List.map (\hue -> List.map (\l -> Okhsl (toFloat hue) 1 l) (reverseRange 0 1 0.005))
-        }
-    )
+    ( {}, Cmd.none )
 
 
 
 -- UPDATE
 
 
-type Msg
-    = Recv { monoSteps : List (List Float), colorSteps : List (List (List Float)) }
+type alias Msg =
+    ()
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Recv d ->
-            let
-                rgbFromList steps =
-                    case steps of
-                        [ r, g, b ] ->
-                            rgb (Basics.round r) (Basics.round g) (Basics.round b)
-
-                        _ ->
-                            rgb 0 0 0
-            in
-            ( { model
-                | monoSteps = List.map rgbFromList d.monoSteps
-                , colorSteps = List.map (\colorSteps -> List.map rgbFromList colorSteps) d.colorSteps
-              }
-            , Cmd.none
-            )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    messageReceiver Recv
+        _ ->
+            ( model, Cmd.none )
 
 
 
@@ -107,27 +59,3 @@ view _ =
         ]
             |> List.map Html.Styled.toUnstyled
     }
-
-
-
--- HELPERS
-
-
-range : Float -> Float -> Float -> List Float
-range lo hi step =
-    rangeHelp lo hi step []
-
-
-rangeHelp : Float -> Float -> Float -> List Float -> List Float
-rangeHelp lo hi step list =
-    if lo <= hi then
-        rangeHelp lo (hi - step) step (hi :: list)
-
-    else
-        list
-
-
-reverseRange : Float -> Float -> Float -> List Float
-reverseRange lo hi step =
-    range lo hi step
-        |> List.reverse
